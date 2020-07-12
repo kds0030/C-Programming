73a53891217e8a4f09b86463e9c768686dbff7f8
WONJAE LIM <67902732+kds0030@users.noreply.github.com>
2020-07-12 22:04:28 +0900

    Create 예제-12(정수형 변수 이해하기(int)).c

---
+++ 예제-12(정수형 변수 이해하기(int)).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    unsigned int j;
+    char k;
+    
+    i = 2000000000;
+    j = 4000000000;
+    k = 'b';
+    
+    printf("정수형 변수 i의 값은 %d\n", i);
+    printf("정수형 변수 j의 값은 %u\n", j);
+    printf("정수형 변수 k의 값은 %d\n", k);
+}
