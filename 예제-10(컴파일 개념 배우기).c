5e60a3ff98c307f0c489a4fb9ce67b8c00fa5d5e
WONJAE LIM <67902732+kds0030@users.noreply.github.com>
2020-07-12 21:41:57 +0900

    Create 예제-10(컴파일 개념 배우기).c

---
+++ 예제-10(컴파일 개념 배우기).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    int hap = 0;
+    
+    
+    for(i = 1; i <= 10; i++){
+        hap += i
+    }
+
+    printf("1부터 10까지의 합:%d", hap);
+}
+// 일부러 ; 일 빼서 오류를 만드는 
